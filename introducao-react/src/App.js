135c58ff540bb0d5821ea696529bfa4410671b7d
Caroline Alves <e.carolinesalves@gmail.com>
2022-01-11 17:33:33 -0300

    Estilizando os hello world

--- introducao-react/src/App.js
+++ introducao-react/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Helloworld (props){
+
+    return (
+        <div className="card">
+            <p className={props.textClass}>{props.setConfig("Hello world!")}</p>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+
+    function normalText (string) {
+        return string;
+    }
+
+    function upperCase (string) {
+        return string.toUpperCase();
+    }
+
+    function lowerCase (string) {
+        return string.toLowerCase();
+    }
+
+    function textReplace (string) {
+        return string.replace("Hello", "HELLO");
+    }
+
+    function textReverse (string) {
+        return string.split("").reverse();
+    }
+
+    return (
+        <div className="App">
+            <Helloworld setConfig={normalText}/>
+            <Helloworld setConfig={normalText} textClass="text-shadow"/>
+            <Helloworld setConfig={textReplace}/>
+            <Helloworld setConfig={normalText} textClass="transparent"/>
+            <Helloworld setConfig={upperCase}/>
+            <Helloworld setConfig={normalText} textClass="text-bold"/>
+            <Helloworld setConfig={lowerCase}/>
+            <Helloworld setConfig={normalText} textClass="text-gradient"/>
+            <Helloworld setConfig={textReverse}/>
+            <Helloworld setConfig={normalText} textClass="background-gradient"/>
+        </div>
+    );
 }
 
 export default App;
