e8187a08d0d0155e47a52a766f946a64be648b5b
Caroline Alves <e.carolinesalves@gmail.com>
2022-01-13 12:29:30 -0300

    Refatorando o código

--- introducao-react/src/App.js
+++ introducao-react/src/App.js
@@ -7,45 +7,89 @@ function Helloworld (props){
 
     return (
         <div className="card">
-            <p className={props.textClass}>{props.setConfig("Hello world!")}</p>
+            <p className={props.textClass}>{props.method("Hello world!")}</p>
             <span className="date">{format(date, "dd-MMMM-yyyy")}</span>
         </div>
     )
 }
 
 function App() {
-    function normalText (string) {
-        return string;
-    }
+    const settings = [
+        {
+            id: 1,
+            method: (string) => {
+                return string;
+            }
+        },
+        {
+            id: 2,
+            method: (string) => {
+                return string;
+            },
+            textClass: "text-shadow"
+        },
+        {
+            id: 3,
+            method: (string) => {
+                return string.replace("Hello", "HELLO")
+            }
+        },
+        {
+            id: 4,
+            method: (string) => {
+                return string;
+            },
+            textClass: "transparent"
+        },
+        {
+            id: 5,
+            method: (string) => {
+                return string.toUpperCase();
+            }
+        },
+        {
+            id: 6,
+            method: (string) => {
+                return string;
+            },
+            textClass: "text-bold"
+        },
+        {
+            id: 7,
+            method: (string) => {
+                return string.toLowerCase();
+            }
+        },
+        {
+            id: 8,
+            method: (string) => {
+                return string;
+            },
+            textClass: "text-gradient"
+        },
+        {
+            id: 9,
+            method: (string) => {
+                return string.split("").reverse();
+            }
+        },
+        {
+            id: 10,
+            method: (string) => {
+                return string;
+            },
+            textClass: "background-gradient"
+        }
+    ]
 
-    function upperCase (string) {
-        return string.toUpperCase();
-    }
-
-    function lowerCase (string) {
-        return string.toLowerCase();
-    }
-
-    function textReplace (string) {
-        return string.replace("Hello", "HELLO");
-    }
-
-    function textReverse (string) {
-        return string.split("").reverse();
-    }
 
     return (
         <div className="App">
-            <Helloworld setConfig={normalText}/>
-            <Helloworld setConfig={normalText} textClass="text-shadow"/>
-            <Helloworld setConfig={textReplace}/>
-            <Helloworld setConfig={normalText} textClass="transparent"/>
-            <Helloworld setConfig={upperCase}/>
-            <Helloworld setConfig={normalText} textClass="text-bold"/>
-            <Helloworld setConfig={lowerCase}/>
-            <Helloworld setConfig={normalText} textClass="text-gradient"/>
-            <Helloworld setConfig={textReverse}/>
-            <Helloworld setConfig={normalText} textClass="background-gradient"/>
+            {
+                settings.map(setting => {
+                    return <Helloworld key={setting.id} {...setting}/>
+                })
+            }
         </div>
     );
 }
